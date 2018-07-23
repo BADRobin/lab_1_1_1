a2068d3f1dddc17bc3a284c5da2505b6385d6e15
BADRobin <olegbryl@gmail.com>
2018-07-23 20:40:24 +0300

    Initial commit

---
+++ src/lab111/MyFileList.java
@@ -0,0 +1,31 @@
+package lab111;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class MyFileList {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            listFiles(".");
+        } else {
+            listFiles(args[0]);
+        }
+    }
+
+    private static void listFiles(String fname) {
+        File file = new File(fname);
+        if (file.isDirectory()) {
+            File[] files = file.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.startsWith("I");
+                }
+            });
+            for (File f : files) {
+                System.out.println(f.getName());
+            }
+        }
+    }
+}
